4926d24fadb42f5888ee50330a358210011357f3
rebago <raymond.bago@fortress-asya.com>
2025-02-10 21:35:37 +0800

    added every second

--- cronjob.go
+++ cronjob.go
@@ -2,30 +2,11 @@ package main
 
 import (
 	"cronjob/service"
-
-	"github.com/robfig/cron/v3"
 )
 
-// Minute	Minute field	0-59
-// Hour	Hour field	0-23
-// Day	Day of month	1-31
-// Month	Month field	1-12
-// Weekday	Day of week	0-6 (0 is Sunday)
 
-// Example patterns:
-// "*/5 * * * *": Every 5 minutes
-// "0 9 * * *": Every day at 9:00 AM
-// "0 0 * * 0": Every Sunday at midnight
 
 func main() {
-	c := cron.New()
-
-	// Schedule a job to run every minute
-	c.AddFunc("*/1 * * * *", service.CallMe)
-
-	// Start the cron scheduler
-	c.Start()
-
-	// Keep the program running
-	select {}
+	service.UpdateEverySecond()
+	service.UpdateEveryMinute()
 }
--- service/function.go
+++ service/function.go
@@ -3,9 +3,52 @@ package service
 import (
 	"fmt"
 	"time"
+
+	"github.com/robfig/cron/v3"
 )
 
-func CallMe() {
-	fmt.Println("Calling the function")
+// Minute	Minute field	0-59
+// Hour	Hour field	0-23
+// Day	Day of month	1-31
+// Month	Month field	1-12
+// Weekday	Day of week	0-6 (0 is Sunday)
+
+// Example patterns:
+// "*/5 * * * *": Every 5 minutes
+// "0 9 * * *": Every day at 9:00 AM
+// "0 0 * * 0": Every Sunday at midnight
+func CallMe(message string) {
+	fmt.Println("Calling the function:", message)
 	fmt.Println("Job executed at", time.Now().Format(time.Kitchen))
+}
+
+func UpdateEverySecond() {
+	// Create a new cron with seconds precision enabled
+	c := cron.New(cron.WithSeconds())
+
+	// Schedule the task to run every second
+	c.AddFunc("*/10 * * * * *", func() {
+		CallMe("Every second")
+	})
+
+	// Start the cron scheduler
+	c.Start()
+
+	// Keep the program running
+	select {}
+}
+
+func UpdateEveryMinute() {
+	c := cron.New()
+
+	// Schedule a job to run every minute
+	c.AddFunc("*/1 * * * *", func() {
+		CallMe("Every minute")
+	})
+
+	// Start the cron scheduler
+	c.Start()
+
+	// Keep the program running
+	select {}
 }
